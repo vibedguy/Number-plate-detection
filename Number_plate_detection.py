5c15f551b0e2f490e771d36c542c6f676408efc7
Vimalan U <118599093+vibedguy@users.noreply.github.com>
2023-11-16 18:23:45 +0530

    Update Number_plate_detection.py

--- Number_plate_detection.py
+++ Number_plate_detection.py
@@ -26,10 +26,10 @@ while True:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             cv2.putText(img,"NumberPlate",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
             imgRoi = img[y:y+h,x:x+w]
-            cv2.imshow("ROI",imgRoi)
+            cv2.imshow("ROI",Scanner)
     cv2.imshow("Result",img)
     if cv2.waitKey(1) & 0xFF ==ord('s'):
-        cv2.imwrite("D:\SACHIN\cascade\IMAGES"+str(count)+".jpg",imgRoi)
+        cv2.imwrite("D:\SACHIN\cascade\IMAGES"+str(count)+".jpg",Scanner)
         cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
         cv2.putText(img,"Scan Saved",(15,265),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),2)
         cv2.imshow("Result",img)
